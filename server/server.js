43e188056c179733fd3dd7165d0c7755e3e4a0c0
Desubicator <francopirinoli@gmail.com>
2023-01-17 20:58:37 -0300

    changes to  server

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
+
+console.log(process.env.OPENAI_API_KEY);
+
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
     });
-
-const openai = new OpenAIApi(configuration);
 
 const app = express();
 app.use(cors());
