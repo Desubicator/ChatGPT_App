ef0720aa02ad87578540f779c7ea99ea1e25c3bd
Desubicator <francopirinoli@gmail.com>
2023-01-17 21:41:49 -0300

    Changes to server for deplyment

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async(e) => {
 
   // Fetch data from server
 
-const response = await fetch("http://localhost:5000", {
+const response = await fetch("https://chatgpt-app-rmbr.onrender.com", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
